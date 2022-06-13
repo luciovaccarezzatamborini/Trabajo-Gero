64297d3d1cc1e67db6d6858f0220b54a48cb8691
luciovaccarezzatamborini <101400339+luciovaccarezzatamborini@users.noreply.github.com>
2022-06-13 14:54:58 -0300

    Initial commit

---
+++ Assets/botoninicio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botoninicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public GameObject player;
+    public GameObject panel;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BotonInicio()
+    {
+        panel.SetActive(false);
+        player.transform.position = new Vector3(0, 1.02f, 5.15f);
+        player.transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float subida;
+    
+
+    float a;
+    public bool isAble;
+    public GameObject prefab;
+    Rigidbody rb;
+    bool jump = true;
+
+    public GameObject panel;
+    void Start()
+    {
+        isAble = false;
+
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) )
+        {
+           
+            // transform.position += new Vector3(0, 0, speed);
+           
+           
+                transform.Translate(0, 0, speed);
+            
+
+
+        }
+
+        
+
+        if (Input.GetKey(KeyCode.S)  )
+        {
+            // transform.position -= new Vector3(0, 0, speed);
+
+            transform.Translate(0, 0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.D) )
+        {
+            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.A) )
+        {
+            transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          
+            if (  jump) {
+
+                jump = false;
+                rb.AddForce(Vector3.up * subida, ForceMode.Impulse);
+
+            }
+
+            // GameObject clon;
+            //clon = Instantiate(prefab);
+            //clon.transform.position = transform.position - new Vector3(0, 0, -1);
+        }
+       a = transform.position.y;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            jump = true;
+        }
+
+        if (col.gameObject.tag == "Respawn")
+        {
+            transform.position = new Vector3(0, 1.02f, 5.15f);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (col.gameObject.tag == "Finish")
+        {
+            panel.SetActive(true);
+        }
+        }
+}
