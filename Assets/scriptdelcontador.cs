aca50b464737eb6bee534750f93a1b9ae2ee8b55
luciovaccarezzatamborini <101400339+luciovaccarezzatamborini@users.noreply.github.com>
2022-06-16 12:09:47 -0300

    Contador cuenta regresiva(faltaa)

---
+++ Assets/scriptdelcontador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scriptdelcontador : MonoBehaviour
+{
+    float tiempoActual;
+    public float tiempoparaelcontador;
+   public Text cuentaregresiva;
+    public GameObject panel;
+    void Start()
+    {
+        tiempoActual = tiempoparaelcontador;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tiempoActual == 0)
+        {
+            panel.SetActive(true);
+        }
+        else
+        {
+            tiempoActual -= 1 * Time.deltaTime;
+            cuentaregresiva.text = Mathf.Floor(tiempoActual).ToString();
+        }
+
+       
+    }
+}
